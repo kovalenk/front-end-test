ccebd8d9cde7b05eb690a55df1cc7a77a18290d5
Kovalenko Yuriy <yura.kovalenko.1996.29@gmail.com>
2019-05-22 14:12:24 +0300

    add stubs for activity data

--- src/app/landing/pages/activity/activity.component.ts
+++ src/app/landing/pages/activity/activity.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import * as contentStub from './stubs/stub';
 
 @Component({
   selector: 'app-activity',
@@ -7,39 +8,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ActivityComponent implements OnInit {
 
-  activityGroup = [
-    {
-      className: 'check',
-      text: 'Darika Samak mark as done Listing on Product Hunt so that we can reach as many potential users',
-      time: '8:40 PM',
-    },
-    {
-      className: 'comment',
-      text: 'Emilee Simchenko commented on Account for teams and personal in bottom style',
-      time: '7:32 PM',
-      description: 'During a project build, it is necessary to evaluate the ' +
-        'product design and development against project requirements and outcomes',
-    },
-    {
-      className: 'download',
-      text: 'Darika Samak uploaded 4 files on An option to search in current projects or in all projects',
-      time: '6:02 PM',
-      images: [
-        {
-          url: 'some url'
-        },
-        {
-          url: 'some url'
-        },
-        {
-          url: 'some url'
-        },
-        {
-          url: 'some url'
-        },
-      ]
-    }
-  ];
+  activityGroup = contentStub.activityData;
 
   constructor() {
   }
---
+++ src/app/landing/pages/activity/stubs/stub.ts
@@ -0,0 +1,33 @@
+export const activityData = [
+  {
+    className: 'check',
+    text: 'Darika Samak mark as done Listing on Product Hunt so that we can reach as many potential users',
+    time: '8:40 PM',
+  },
+  {
+    className: 'comment',
+    text: 'Emilee Simchenko commented on Account for teams and personal in bottom style',
+    time: '7:32 PM',
+    description: 'During a project build, it is necessary to evaluate the ' +
+      'product design and development against project requirements and outcomes',
+  },
+  {
+    className: 'download',
+    text: 'Darika Samak uploaded 4 files on An option to search in current projects or in all projects',
+    time: '6:02 PM',
+    images: [
+      {
+        url: 'some url'
+      },
+      {
+        url: 'some url'
+      },
+      {
+        url: 'some url'
+      },
+      {
+        url: 'some url'
+      },
+    ]
+  }
+];
